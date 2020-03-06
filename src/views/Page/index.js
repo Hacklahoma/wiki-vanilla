262f5aba9194662934ecc1506a29db135828b562
s-fletcher <sfletchertx@gmail.com>
2020-03-06 16:15:14 -0600

    initial commit

--- src/views/Page/index.js
+++ src/views/Page/index.js
@@ -2,16 +2,6 @@ import React, { Component } from "react";
 import firebase from "../../config/firebase";
 import PageHeader from "../../components/PageHeader";
 import "./index.scss";
-import EditorJS from "@editorjs/editorjs";
-import Header from "@editorjs/header";
-import LinkTool from "@editorjs/link";
-import Warning from "@editorjs/warning";
-import List from "@editorjs/list";
-import InlineCode from "@editorjs/inline-code";
-import CodeTool from '@editorjs/code'
-import Checklist from '@editorjs/checklist'
-import Delimiter from '@editorjs/delimiter'
-import Marker from '@editorjs/marker'
 import { withRouter } from "react-router-dom";
 
 class Page extends Component {
@@ -20,7 +10,6 @@ class Page extends Component {
         this.state = {
             name: "",
             doc: null,
-            editor: null,
             exists: false,
         };
     }
@@ -52,79 +41,7 @@ class Page extends Component {
                 this.props.history.push("/404");
                 return;
             }
-
-            // Setting up editor
-            const editor = new EditorJS({
-                /**
-                 * Id of Element that should contain the Editor
-                 */
-                holderId: "codex-editor",
-                /**
-                 * Available Tools list.
-                 * Pass Tool's class or Settings object for each Tool you want to use
-                 */
-                tools: {
-                    // Header tool
-                    header: {
-                        class: Header,
-                        config: {
-                            placeholder: "Enter a header",
-                            levels: [1, 2, 3, 4],
-                            defaultLevel: 1,
-                        },
-                    },
-                    list: {
-                        class: List,
-                        inlineToolbar: true,
-                    },
-                    inlineCode: {
-                        class: InlineCode,
-                        shortcut: "CMD+SHIFT+M",
-                    },
-                    marker: {
-                        class: Marker,
-                        shortcut: "CMD+SHIFT+M",
-                    },
-                    checklist: {
-                        class: Checklist,
-                        inlineToolbar: true,
-                    },
-                    delimiter: Delimiter,
-                    code: CodeTool,
-                },
-                /**
-                 * Previously saved data that should be rendered
-                 */
-                data: doc.data().data,
-                /**
-                 * onReady callback
-                 */
-                onReady: () => {
-                    console.log("Editor.js is ready to work!");
-                },
-                /**
-                 * Fires when something changed in DOM
-                 */
-                onChange: () => {
-                    this.saveData();
-                },
-            });
-            // Saving editor state
-            this.setState({
-                editor: editor,
-            });
         });
-    }
-
-    saveData() {
-        this.state.editor
-            .save()
-            .then(outputData => {
-                this.state.doc.update({ data: outputData });
-            })
-            .catch(error => {
-                console.log("Saving failed: ", error);
-            });
     }
 
     render() {
