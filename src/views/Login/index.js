442a333e139a3385d0f491e5c0a038c447d925ef
s-fletcher <sfletchertx@gmail.com>
2020-03-20 17:22:58 -0500

    New login screen

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,18 +1,38 @@
 import React, { Component } from "react";
+import "./index.scss";
+import logo from "../../assets/logo.png";
 
 export class Login extends Component {
     render() {
         return (
             <div className="Login">
-                <a href={"https://slack.com/oauth/authorize?scope=identity.basic&client_id=" + process.env.REACT_APP_SLACK_CLIENT_ID}>
-                    <img
-                        alt="Sign in with Slack"
-                        height="40"
-                        width="172"
-                        src="https://platform.slack-edge.com/img/sign_in_with_slack.png"
-                        srcSet="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x"
-                    />
-                </a>
+                <div className="header">
+                    <a href="/">
+                        <div className="logo">
+                            <img src={logo} alt="logo" />
+                            <p>Wiki</p>
+                        </div>
+                    </a>
+                </div>
+                <div className="container">
+                    <div className="content">
+                        <h1>Hacklahoma's team knowledge base</h1>
+                        <a
+                            href={
+                                "https://slack.com/oauth/authorize?scope=identity.basic&client_id=" +
+                                process.env.REACT_APP_SLACK_CLIENT_ID
+                            }
+                        >
+                            <img
+                                alt="Sign in with Slack"
+                                height="40"
+                                width="172"
+                                src="https://platform.slack-edge.com/img/sign_in_with_slack.png"
+                                srcSet="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x"
+                            />
+                        </a>
+                    </div>
+                </div>
             </div>
         );
     }
