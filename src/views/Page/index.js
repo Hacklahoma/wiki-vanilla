89431676e57f1943f2989026caf3190d1e98cc40
s-fletcher <sfletchertx@gmail.com>
2020-03-20 02:00:51 -0500

    Implemented table of contents

--- src/views/Page/index.js
+++ src/views/Page/index.js
@@ -21,6 +21,7 @@ class Page extends React.Component {
         };
         this.handleChange = this.handleChange.bind(this);
         this.toggleEdit = this.toggleEdit.bind(this);
+        this.goTo = this.goTo.bind(this);
     }
 
     componentDidMount() {
@@ -84,7 +85,109 @@ class Page extends React.Component {
     // Update database and state when value changes
     handleChange(value) {
         this.setState({ text: value });
+        if (
+            value === "<p><br></p>" ||
+            value === "<h1><br></h1>" ||
+            value === "<h2><br></h2>" ||
+            value === "<h3><br></h3>"
+        ) {
+            value = "";
+        }
         this.state.doc.update({ data: value });
+    }
+
+    // Scrolls to header that is clicked in the ToC
+    goTo(event, tag) {
+        for (var element of document.getElementsByClassName("quill")[0].getElementsByTagName(tag)) {
+            if (element.innerHTML === event.target.innerHTML) {
+                element.scrollIntoView();
+            }
+        }
+    }
+
+    // Renders table of contents
+    renderToC() {
+        var parser = new DOMParser();
+        var htmlDoc = parser.parseFromString(this.state.text, "text/html");
+        var result = [];
+        var level;
+        // Iterates through text and assigns column
+        for (var item of htmlDoc.getElementsByTagName("*")) {
+            if (item.innerHTML === "<br>") {
+                continue;
+            }
+            if (item.tagName === "H1") {
+                result.push(
+                    <p
+                        key={Math.random()}
+                        onClick={e => this.goTo(e, "h1")}
+                        className="toc-1 toc-item"
+                    >
+                        {item.innerHTML}
+                    </p>
+                );
+                level = 1;
+            } else if (item.tagName === "H2") {
+                if (level === 1 || level === 2 || level === 3) {
+                    result.push(
+                        <p
+                            key={Math.random()}
+                            onClick={e => this.goTo(e, "h2")}
+                            className="toc-2 toc-item"
+                        >
+                            {item.innerHTML}
+                        </p>
+                    );
+                    level = 2;
+                } else {
+                    result.push(
+                        <p
+                            key={Math.random()}
+                            onClick={e => this.goTo(e, "h2")}
+                            className="toc-1 toc-item"
+                        >
+                            {item.innerHTML}
+                        </p>
+                    );
+                    level = 0;
+                }
+            } else if (item.tagName === "H3") {
+                if (level === 0 || level === 1) {
+                    result.push(
+                        <p
+                            key={Math.random()}
+                            onClick={e => this.goTo(e, "h3")}
+                            className="toc-2 toc-item"
+                        >
+                            {item.innerHTML}
+                        </p>
+                    );
+                } else if (level === 2 || level === 3) {
+                    result.push(
+                        <p
+                            key={Math.random()}
+                            onClick={e => this.goTo(e, "h3")}
+                            className="toc-3 toc-item"
+                        >
+                            {item.innerHTML}
+                        </p>
+                    );
+                } else {
+                    result.push(
+                        <p
+                            key={Math.random()}
+                            onClick={e => this.goTo(e, "h3")}
+                            className="toc-1 toc-item"
+                        >
+                            {item.innerHTML}
+                        </p>
+                    );
+                    level = 0;
+                }
+            }
+        }
+        if (result.length > 0) return <div className="toc">{result}</div>;
+        else return null;
     }
 
     // Toggle between read only mode and edit
@@ -109,6 +212,7 @@ class Page extends React.Component {
             return (
                 <div className="Page container">
                     <PageHeader to="home" title={this.state.title} />
+                    {this.renderToC()}
                     <ReactQuill
                         placeholder="There is nothing here..."
                         readOnly={this.state.readOnly}
